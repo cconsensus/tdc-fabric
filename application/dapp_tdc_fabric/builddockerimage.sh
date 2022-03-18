7c4ed9c3555be5575e9cae50a9ec575456af5b64
David Faulstich Diniz Reis <davidfdr@gmail.com>
2022-03-18 19:42:16 +0000

    shells for building and starting images
    Signed-off-by: David Faulstich Diniz Reis <davidfdr@gmail.com>

---
+++ application/dapp_tdc_fabric/builddockerimage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ----------
+# SPDX-License-Identifier: Apache-2.0
+# ----------
+#
+docker build --tag cconsensus/dapp_tdc_fabric:1.0.0 .
---
+++ application/dapp_tdc_fabric/docker/dapp-api-start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ----------
+# SPDX-License-Identifier: Apache-2.0
+# ----------
+#
+docker-compose -f ./docker-compose-dapps.yaml up -d
+
+sleep 3
+docker-compose ps
